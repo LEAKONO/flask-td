1719334ecfd9f61224439ccb9b6eca1d7bfa3ae8
LEAKONO <leakonoemmanuel3@gmail.com>
2025-05-21 21:08:50 +0300

    Define SQLAlchemy models for User and Task with relationships

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from config import Config
 from models import db, bcrypt
 from auth import auth
+from routes import routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
 bcrypt.init_app(app)
+JWTManager(app)
 
 app.register_blueprint(auth, url_prefix='/auth')
+app.register_blueprint(routes, url_prefix='/api')
 
 if __name__ == '__main__':
     with app.app_context():
